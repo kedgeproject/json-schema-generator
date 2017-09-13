aac625b561cf45b0e93ea4601dec3311d2a92d23
Suraj Deshmukh <surajssd009005@gmail.com>
2017-09-13 15:29:35 +0530

    download spec.go from github from upstream

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,13 +3,11 @@
 set -xe
 
 curl -O https://raw.githubusercontent.com/kubernetes/kubernetes/$(curl https://raw.githubusercontent.com/surajssd/kedgeSchema/master/scripts/k8s-release)/api/openapi-spec/swagger.json
-# TODO: replace the download link below with the link from the upstream one
-#curl -O https://raw.githubusercontent.com/surajssd/kedgeSchema/master/spec.go
-#curl -O https://raw.githubusercontent.com/surajssd/kedge/31ceefdb5340fdd9e714854f91e1f839e5fcb4f2/pkg/spec/spec.go
+curl -O https://raw.githubusercontent.com/kedgeproject/kedge/master/pkg/spec/spec.go
 
 kedgeSchemaGen > output.json
 
 mkdir -p configs
 openapi2jsonschema output.json -o configs/ --stand-alone
 
-rm -rf swagger.json output.json
+rm -rf swagger.json output.json spec.go
