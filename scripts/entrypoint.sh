bef69da25f24797056f8b28f579430417adac2d2
Suraj Deshmukh <surajssd009005@gmail.com>
2017-09-14 19:30:52 +0530

    update links to the new repo

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -xe
 
-curl -O https://raw.githubusercontent.com/kubernetes/kubernetes/$(curl https://raw.githubusercontent.com/surajssd/kedgeSchema/master/scripts/k8s-release)/api/openapi-spec/swagger.json
+curl -O https://raw.githubusercontent.com/kubernetes/kubernetes/$(curl https://raw.githubusercontent.com/kedgeproject/kedge-jsonschema/master/scripts/k8s-release)/api/openapi-spec/swagger.json
 curl -O https://raw.githubusercontent.com/kedgeproject/kedge/master/pkg/spec/spec.go
 
 kedge-jsonschema > output.json
